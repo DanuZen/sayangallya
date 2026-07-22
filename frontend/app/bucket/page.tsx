6d43ab21c8f6f297e792156fae8a6e2adb3c5b5d
DanuZen <wira.10.11.04@gmail.com>
2026-07-22 18:46:20 +0700

    feat: add Bucket List page

---
+++ frontend/app/bucket/page.tsx
@@ -0,0 +1,9 @@
+import BucketList from "@/components/BucketList";
+
+export default function BucketPage() {
+  return (
+    <main className="min-h-screen">
+      <BucketList />
+    </main>
+  );
+}
