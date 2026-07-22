b92e8c8b9a13df7a40f07aba9dc70bcf6b1d8929
DanuZen <wira.10.11.04@gmail.com>
2026-07-22 18:46:26 +0700

    feat: add Couple Games page

---
+++ frontend/app/game/page.tsx
@@ -0,0 +1,9 @@
+import CoupleGames from "@/components/CoupleGames";
+
+export default function GamePage() {
+  return (
+    <main className="min-h-screen bg-[#F3EAE3]">
+      <CoupleGames />
+    </main>
+  );
+}
